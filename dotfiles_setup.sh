d3558428a757a72ffa84c0479eae835f7c2a3785
Thomas <crecy.thomas@gmail.com>
2025-04-10 09:25:32 +0200

    script dotfiles

--- dotfiles_setup.sh
+++ dotfiles_setup.sh
@@ -21,3 +21,22 @@ echo
 
 # Exit on any error
 set -e
+
+# Starship setup
+echo ">> Setting up starship prompt..."
+cp dotfiles/starship.toml ~/.config/starship.toml
+echo ">> Starship prompt setup complete."
+
+# bash setup
+echo ">> Setting up bash config..."
+cp dotfiles/.bashrc ~/.bashrc
+echo ">> Bash config complete."
+
+# kitty setup
+echo ">> Setting up kitty config..."
+mkdir -p ~/.config/kitty
+mkdir -p ~/.config/kitty/kitty-themes/themes
+cp dotfiles/kitty/kitty.conf ~/.config/kitty/kitty.conf
+cp dotfiles/kitty/theme.conf ~/.config/kitty/theme.conf
+cp dotfiles/kitty/gruvbox_dark.conf ~/.config/kitty/kitty-themes/themes/
+echo ">> Kitty config complete."
